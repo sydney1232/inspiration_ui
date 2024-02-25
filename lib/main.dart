e8897dcb42a1a1515ea907b5d9d98d04de4f1180
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-02-25 12:40:54 +0800

    Added gradient for images im ListView

--- lib/main.dart
+++ lib/main.dart
@@ -123,12 +123,24 @@ class _MyHomePageState extends State<MyHomePage> {
       aspectRatio: 2 / 3,
       child: Container(
         decoration: BoxDecoration(
-            color: Colors.orange,
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(image),
-            )),
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+                0.2,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1)
+              ])),
+        ),
       ),
     );
   }
